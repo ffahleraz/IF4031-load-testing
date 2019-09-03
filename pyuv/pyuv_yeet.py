c2562be0109bf8f07a09161e09b4a389cdfa7618
diwangs <27280516+diwangs@users.noreply.github.com>
2019-09-03 18:45:20 +0700

    Delete comments

--- pyuv/pyuv_yeet.py
+++ pyuv/pyuv_yeet.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 
 import signal
 import pyuv
-import time
+# import time
 
 server = None
 signal_h = None
@@ -12,7 +12,6 @@ def main():
     print("PyUV version %s" % pyuv.__version__)
 
     loop = pyuv.Loop.default_loop()
-    # clients = []
 
     server = pyuv.TCP(loop)
     server.bind(("0.0.0.0", 1234))
@@ -27,18 +26,16 @@ def main():
 def on_connection(server, error):
     client = pyuv.TCP(server.loop)
     server.accept(client)
-    # clients.append(client)
     with open("test-pages/500b.html", "rb") as file:
         client.write(file.read())
-    # client.write("Yeet\n".encode('ascii'))
     client.shutdown(on_shutdown)
 
 def on_shutdown(client, err):
+    # time.sleep(0.005) 
     client.close()
 
 def signal_cb(handle, signum):
     global server, signal_h
-    # [c.close() for c in clients]
     signal_h.close()
     server.close()
 
